e93d21eee85720415458ec0d531ea18e729bbdb6
Juan Vargas <juandeveloper19@gmail.com>
2022-04-21 04:07:20 -0500

    Se ejecuto el metodo que muestra el juego y se creo la funcion que arranca la aplicacion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // Importacion de clases
-import Board from './class/board';
-import Ball from './class/ball';
-import Bar from './class/bar';
-import BoardView from './class/boardView';
+import Board from './class/board.js';
+import Ball from './class/ball.js';
+import Bar from './class/bar.js';
+import BoardView from './class/boardView.js';
 
 // Instancias de clases
 const board = new Board(800, 400);
@@ -10,3 +10,14 @@ const ball = new Ball(350, 100, 10, board);
 const bar = new Bar(20, 100, 20, 100, board);
 const barTwo = new Bar(760, 100, 20, 100, board);
 const boardView = new BoardView(canvas, board);
+
+// Primera vista del juego
+boardView.draw();
+
+// Inicio del juego
+const main = () => {
+  boardView.play();
+  window.requestAnimationFrame(main);
+};
+
+window.requestAnimationFrame(main);
